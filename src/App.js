67b19eeb1a7c56a4b0413de7cbabfc5b3c79808e
Husain Hidayah <husain.fikri@gmail.com>
2023-02-23 10:42:30 +0000

    add react app

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <form
+        name="contact v2"
+        method="post"
+        data-netlify="true"
+        onSubmit="submit"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="contact v2" />
+
+        <div hidden>
+          <input name="bot-field" />
+        </div>
+
+        <div>
+          <label>
+            First name
+            <br />
+            <input type="text" name="first-name" />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Last name
+            <br />
+            <input type="text" name="last-name" />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input id="email" type="email" name="email" />
+        </div>
+
+        <div>
+          <label>
+            Any Comments?
+            <br />
+            <textarea name="comments"></textarea>
+          </label>
+        </div>
+
+        <button type="submit">Submit The Results</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
